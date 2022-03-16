16ec9ef2a8fd0a5e8220efbc735962eae37eca54
wuhelong <5387348+wuhelong@user.noreply.gitee.com>
2022-03-16 16:39:46 +0800

    完成红包日志ui和信息提醒和信息提醒详情

--- pages/message_list/message_list.js
+++ pages/message_list/message_list.js
@@ -1,66 +1,87 @@
 // pages/message_list/message_list.js
+const app = getApp();
 Page({
-
   /**
    * 页面的初始数据
    */
   data: {
-
+    message_list: [],
+    page: 1,
+    loading_text: "加载更多...",
+    noMore_text: "无更多数据",
+    loading: true,
+    noMore: false,
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+    this.getMessageList();
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
-  onReady: function () {
+  onReady: function () {},
 
+  getMessageList() {
+    // 获取列表数据
+    let token = wx.getStorageSync("token");
+    this.setData({
+      loading: true,
+    });
+    wx.request({
+      url: app.globalData.baseUrl + "member/notice/get_notice",
+      method: "POST",
+      data: {
+        token: token,
+        page: this.data.page,
+        pagesize: 10,
+      },
+      success: (res) => {
+        let data = res.data;
+        if (data.code === 1) {
+          this.setData({
+            message_list: this.data.message_list.concat(data.data.list),
+            page: data.data.cur_page.page + 1,
+            loading: false,
+          });
+        } else if (data.code === 210) {
+          this.setData({
+            noMore: true,
+            loading: false,
+          });
+        } else if (data.code === 510) {
+          //token 过期的情况
+          wx.showToast({
+            title: data.msg,
+            icon: "error",
+            duration: 2000,
+          });
+          setTimeout(() => {
+            wx.navigateTo({ url: "../login/login?operation=relogin" });
+          }, 300);
+        }
+      },
+    });
+  },
+  openMessageDetail(e) {
+    let id = e.currentTarget.dataset.id;
+    wx.navigateTo({
+      url: `/pages/message_detail/message_detail?id=${id}`,
+    });
   },
 
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
   onReachBottom: function () {
-
+    if (this.data.noMore) {
+      return;
+    }
+    this.getMessageList();
   },
 
   /**
    * 用户点击右上角分享
    */
-  onShareAppMessage: function () {
-
-  }
-})
+  onShareAppMessage: function () {},
+});
